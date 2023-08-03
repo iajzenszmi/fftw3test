d1a3f678ac9acf0d3af3cd7528a6a9b261363976
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2023-08-04 03:46:02 +1000

    A complete and working fftwtest2.f
    Fast Fourier Transform in Fortran 77 A complete and working fftwtest2.f

---
+++ fftwtest2.f
@@ -0,0 +1,13 @@
+        double precision in
+        integer M /16/
+        integer N /2/
+        dimension in(16,2)
+        double complex out
+        dimension out(16/2 + 1, 16)
+        integer*8 plan
+
+        call dfftw_plan_dft_r2c_2d(plan,M,N,in,out,FFTW_ESTIMATE)
+        call dfftw_execute_dft_r2c(plan, in, out)
+        print *,  in, out
+        call dfftw_destroy_plan(plan)
+        end program
